8848acaa4edb90d599dbc2ba411f3688ef519208
panu2306 <pranavbhendawade@gmail.com>
2020-03-25 19:18:50 +0530

    minor change in code

--- binary_queries.py
+++ binary_queries.py
@@ -4,15 +4,21 @@ l = [int(x) for x in input().split()][:N]
 
 while(Q!=0):
 	q = [int(x) for x in input().split()]
+	
 	if(q[0]==0):
-	        s = l[q[1]:q[2]]
-        	sum = 0
-        	for i in reversed(range(len(s))):
-                	sum += (2**(len(s)-1-i))*s[i]
-        	if(sum%2 == 0):
-                	print("EVEN")
-        	else:
+		s = l[q[1]:q[2]]
+		sum = 0
+		length = len(s)
+        	
+		for i in range(length):
+			if(s[i]!=0):
+				sum += (2**(length-1-i))*s[i]
+		
+		if(sum%2 == 0):
+			print("EVEN")	
+		else:
                 	print("ODD")
+
 	else:
         	if(l[q[1]] == 0):
                 	l[q[1]] = 1
